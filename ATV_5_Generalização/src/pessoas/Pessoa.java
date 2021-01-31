e29aa5b327d0c1b78c2120b93ac00333e59ef012
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-01-31 11:47:54 -0300

    update ATV_5 SRC_pessoas v1.9
    Criação e update da atv 1 pessoa

---
+++ ATV_5_Generalização/src/pessoas/Pessoa.java
@@ -0,0 +1,55 @@
+package pessoas;
+
+public class Pessoa {
+	
+	//CONSTRUTORES
+	public Pessoa() {
+		
+	}
+	
+	public Pessoa(String nome, String endereco) {
+		setNome(nome);
+		setEndereco(endereco);
+	}
+	
+	
+	//ATRIBUTOS
+	private String nome;
+	private String endereco;
+	
+	//GETTERS E SETTERS
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		if(endereco.length() > 0) {
+			this.endereco = endereco;
+		}
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		if(nome.length() > 0) {
+			this.nome = nome;
+		}
+	}
+	
+	
+	//toSTRING
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Pessoa [nome=");
+		builder.append(nome);
+		builder.append(", endereco=");
+		builder.append(endereco);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
+	
+	
+}
---
+++ ATV_5_Generalização/src/pessoas/PessoaFisica.java
@@ -0,0 +1,57 @@
+package pessoas;
+
+public class PessoaFisica extends Pessoa {
+	
+	//ATRIBUTOS
+	private String cpf;
+	private String estadoCivil;
+	
+	//CONSTRUTORES
+	public PessoaFisica() {
+		super();
+	}
+	
+	public PessoaFisica(String nome, String endereco) {
+		super(nome, endereco);
+	}
+	
+	public PessoaFisica(String nome, String endereco, String cpf, String estadoCivil) {
+		super(nome, endereco);
+		setCpf(cpf);
+		setEstadoCivil(estadoCivil);
+	}
+	
+	//GETTERS E SETTERS
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		if (cpf.length() > 0) {
+			this.cpf = cpf;
+		}
+	}
+	
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(String estadoCivil) {
+		if(estadoCivil.length() > 0) {
+			this.estadoCivil = estadoCivil;
+		}
+	}
+	
+	//TO_STRING
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("PessoaFisica [cpf=");
+		builder.append(cpf);
+		builder.append(", estadoCivil=");
+		builder.append(estadoCivil);
+		builder.append(", toString()=");
+		builder.append(super.toString());
+		builder.append("]");
+		return builder.toString();
+	}
+		
+}
