8296bd2f5bc3e5ae004699bf3c5c529d94707645
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-01-31 18:27:10 -0300

    update atv3 v2.0

--- ATV_5_Generalização/src/atv3_simples/main.java
+++ ATV_5_Generalização/src/atv3_simples/main.java
@@ -14,13 +14,33 @@ public class main {
 		CS.saque(100);
 		System.out.println(CS);
 		
+		ContaSimples CS2 = new ContaSimples("Banco Brasileiro", 2036, 789562, 1000);
+		CS2.setSaldoPoupanca(2000);
+		CS2.saquePoupanca(99);
+		System.out.println(CS2);
 		
+		ContaSimples CS3 = new ContaSimples("Banco Master", 0367, 92001, 40, 600);
+		System.out.println(CS3);
 		
+		System.out.println("\n");
 		
+		//CONTA ESPECIAL
+		ContaEspecial CE = new ContaEspecial();
+		CE.setBanco("Nubanco");
+		CE.setAgencia(3256);
+		CE.setNumerodaconta(7851256);
+		CE.setSaldo(2530);
+		CE.setDiasSemJuros(5);
+		CE.setLimite(3000);
+		System.out.println(CE);
 		
+		ContaEspecial CE2 = new ContaEspecial("PixPay", 5027, 9635, 7000);
+		CE2.setDiasSemJuros(20);
+		CE2.setLimite(20000);
+		System.out.println(CE2);
 		
-		
-		
+		ContaEspecial CE3 = new ContaEspecial("HP investimentos", 9630, 102335, 90000, 25, 100000);
+		System.out.println(CE3);
 
 	}
 
