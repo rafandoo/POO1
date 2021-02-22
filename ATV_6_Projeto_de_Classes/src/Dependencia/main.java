5106239bdf9149447ce1e8c8a701bb81a966871a
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-02-21 23:07:05 -0300

    update fv2

--- ATV_6_Projeto_de_Classes/src/Dependencia/main.java
+++ ATV_6_Projeto_de_Classes/src/Dependencia/main.java
@@ -3,7 +3,14 @@ package Dependencia;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Lampada lamp = new Lampada();
+		lamp.setPotencia(10);
+		
+		Interruptor intr = new Interruptor();
+		
+		intr.ligarInterp(lamp);
+		System.out.println(intr);
 
 	}
 
