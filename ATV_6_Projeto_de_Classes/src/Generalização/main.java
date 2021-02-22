3287172199547560017bfbcb65fead3d5d2cf2e0
Rafael Camargo <rafaelcamargo.inf@gmail.com>
2021-02-21 23:25:14 -0300

    update v1.9

---
+++ ATV_6_Projeto_de_Classes/src/Generalização/main.java
@@ -0,0 +1,12 @@
+package Generalização;
+
+public class main {
+
+	public static void main(String[] args) {
+		Moto m = new Moto("honda", "2009", 125);
+		System.out.println(m);
+		
+		Carro c = new Carro("chevrolet", "2017", 800, 526);
+		System.out.println(c);
+	}
+}
